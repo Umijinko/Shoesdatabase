d93b04691430147d7f7932d9f64ad381dd06a66f
Umijinko <42897791+TaichiOnoe@users.noreply.github.com>
2019-01-23 17:19:57 +0900

    update_v0.3
    Add card.vue

---
+++ components/Card.vue
@@ -0,0 +1,57 @@
+<template>
+            <v-card color="white" class="black--text">
+              <v-layout row>
+                <v-flex xs7>
+                  <v-card-title primary-title>
+                    <div>
+                      <div class="headline">
+                          <slot name='title'></slot>
+                      </div>
+                      
+                      <slot name='text'></slot>
+
+                    </div>
+                  </v-card-title>
+                </v-flex>
+                <v-flex xs5>
+                  <v-img src="https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg" height="125px" contain></v-img>
+                </v-flex>
+              </v-layout>
+              <v-divider light></v-divider>
+
+              <v-card-actions>
+
+                <v-btn justify-center icon @click="show = !show">
+                  <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
+                </v-btn>
+
+              </v-card-actions>
+
+              <v-slide-y-transition>
+                <v-card-text v-show="show">
+                  <v-flex text-xs-center>
+                    <router-link to="/">
+                      <v-btn class="mx-0" color="primary" large>
+                        <v-icon fab>link</v-icon>
+                      </v-btn>
+                    </router-link>
+                  </v-flex>
+                </v-card-text>
+              </v-slide-y-transition>
+
+            </v-card>
+
+</template>
+
+<script>
+
+export default {
+  name:'card',
+  data(){
+  return{
+      show:false
+    }
+  }
+}
+
+</script>
--- pages/list.vue
+++ pages/list.vue
@@ -1,65 +1,35 @@
 <template>
-  <div id="e3" style="max-width: 1000px; margin: auto;" class="grey lighten-3">
-    <v-card>
+  <div id="e3" style="max-width: 1000px; margin: auto;" >
       <v-container fluid grid-list-lg>
         <v-layout row wrap>
           <v-flex xs12
           v-for='data in myjson'
           :key='data.id'
           >
-            <v-card color="white" class="black--text">
-              <v-layout row>
-                <v-flex xs7>
-                  <v-card-title primary-title>
-                    <div>
-                      <div class="headline">{{data.title}}</div>
-                      <span>test text</span>
-                    </div>
-                  </v-card-title>
-                </v-flex>
-                <v-flex xs5>
-                  <v-img src="https://upload.wikimedia.org/wikipedia/commons/a/ac/No_image_available.svg" height="125px" contain></v-img>
-                </v-flex>
-              </v-layout>
-              <v-divider light></v-divider>
+          <card>
+            <div slot='title'>{{data.title}}
+              </div>
+              <div slot='text'>{{data.text}}
+                </div>
+                
+                </card>
 
-              <v-card-actions>
-                <v-btn icon @click="show = !show">
-                  <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
-                </v-btn>
-              </v-card-actions>
-
-              <v-slide-y-transition>
-                <v-card-text v-show="show">
-                  <v-flex text-xs-center>
-                    <router-link to="/">
-                      <v-btn class="mx-0" color="primary" large>
-                        <v-icon fab>link</v-icon>
-                      </v-btn>
-                    </router-link>
-                  </v-flex>
-                </v-card-text>
-              </v-slide-y-transition>
-
-            </v-card>
           </v-flex>
         </v-layout>
       </v-container>
-    </v-card>
   </div>
 </template>
 
 <script>
-import axios from "axios";
 
-var json ='assets\data.json'
+import json from '../assets/data.json'
+import Card from '../components/Card.vue'
 
 export default {
   name:'All',
   data(){
   return{
       myjson:json,
-      show: "false"
     }
   },
   // mounted(){
@@ -70,12 +40,15 @@ export default {
 
   //   },
 
-  // methods:{
+ components:{
+   'card':Card
+ },
+
+
   //   // push: function(markers){
   //   // Items.forEach(function(item){
   //   // markers.push({position: {lat:item.lat, lng:item.lng}})
   // })
   //   }
-  // }
 };
 </script>
