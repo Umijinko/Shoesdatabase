13c56dd535f56c78db9e15e9cc5ea8168f7a088b
Umijinko <42897791+TaichiOnoe@users.noreply.github.com>
2019-01-24 07:32:05 +0900

    update v0.3

---
+++ pages/list.vue
@@ -0,0 +1,42 @@
+<template>
+  <div id="e3" style="max-width: 1000px; margin: auto;" >
+      <v-container fluid grid-list-lg>
+        <v-layout row wrap>
+          <v-flex xs12
+          v-for='data in myjson'
+          :key='data.id'
+          >
+
+          <card>
+            <div slot='title'>{{data.title}}
+              </div>
+              <div slot='text'>{{data.text}}
+                </div>
+                
+                </card>
+
+          </v-flex>
+        </v-layout>
+      </v-container>
+  </div>
+</template>
+
+<script>
+
+import json from '../assets/data.json'
+import Card from '../components/Card.vue'
+
+export default {
+  name:'All',
+  data(){
+  return{
+      myjson:json,
+    }
+  },
+
+ components:{
+   'card':Card
+ }
+
+};
+</script>
