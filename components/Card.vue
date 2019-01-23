9e3595b1f9e403c305af8b69de967cd5990b6611
Umijinko <TaichiOnoe@users.noreply.github.com>
2019-01-24 02:14:08 +0900

    init

--- components/Card.vue
+++ components/Card.vue
@@ -1,19 +1,15 @@
 <template>
-  <div id="e3" style="max-width: 1000px; margin: auto;" class="grey lighten-3">
-    <v-card>
-      <v-container fluid grid-list-lg>
-        <v-layout row wrap>
-          <v-flex xs12
-          v-for='data in myjson'
-          :key='data.id'
-          >
             <v-card color="white" class="black--text">
               <v-layout row>
                 <v-flex xs7>
                   <v-card-title primary-title>
                     <div>
-                      <div class="headline">{{data.title}}</div>
-                      <span>test text</span>
+                      <div class="headline">
+                          <slot name='title'></slot>
+                      </div>
+                      
+                      <slot name='text'></slot>
+
                     </div>
                   </v-card-title>
                 </v-flex>
@@ -24,9 +20,11 @@
               <v-divider light></v-divider>
 
               <v-card-actions>
-                <v-btn icon @click="show = !show">
+
+                <v-btn justify-center icon @click="show = !show">
                   <v-icon>{{ show ? 'keyboard_arrow_down' : 'keyboard_arrow_up' }}</v-icon>
                 </v-btn>
+
               </v-card-actions>
 
               <v-slide-y-transition>
@@ -42,40 +40,18 @@
               </v-slide-y-transition>
 
             </v-card>
-          </v-flex>
-        </v-layout>
-      </v-container>
-    </v-card>
-  </div>
+
 </template>
 
 <script>
-import axios from "axios";
-
-var json ='assets\data.json'
 
 export default {
-  name:'All',
+  name:'card',
   data(){
   return{
-      myjson:json,
-      show: "false"
+      show:false
     }
-  },
-  // mounted(){
-  //   axios.get(json_url).then(res=>{this.myjson=res.data})
-  // }
-  //   created () {
-  //    this.push(this.markers)
+  }
+}
 
-  //   },
-
-  // methods:{
-  //   // push: function(markers){
-  //   // Items.forEach(function(item){
-  //   // markers.push({position: {lat:item.lat, lng:item.lng}})
-  // })
-  //   }
-  // }
-};
 </script>
